70faae37ff17c4b09f0fc1ad77e8ae29183cf5de
santoseds <santos.junior@academico.ifpb.edu.br>
2023-07-26 06:04:22 -0300

    services update

--- src/services/api.js
+++ src/services/api.js
@@ -1,23 +1,3 @@
-export const createProduct=  async (product)=> {
-        let newProduct= {};
-        await fetch('http://localhost:3001/foods/', {
-          method:'POST',
-          headers: {
-            'content-type': 'application/json',
-          },
-          body: JSON.stringify({
-            emoji: product.emoji,
-            name: product.name,
-            price: product.price     
-            
-          })}).then(res=>res.json()).then(data=> {
-            newProduct = data;
-            console.log('new product tta ok!' + newProduct.name);
-            }
-          ).catch(e=>'Ocorreu erro ao criar com api create!');    
-            
-}   
-
 export const updateProduct = async (produc, formProduct)=>{
   let updated ={};
   await fetch(`http://localhost:3001/foods/${produc.id}`, {
@@ -33,7 +13,7 @@ export const updateProduct = async (produc, formProduct)=>{
   }).then(res => res.json()).then(data=> {
       updated = data;
       console.log('este é o updated: ' + updated.name)}
-    ).catch(e=>'erro no updateProduct').finally(()=>'Finally está no console!');
+    ).catch(e=>{}).finally(()=>{});
 }
 
 export const deleteProduct= async (product)=>{
@@ -46,5 +26,5 @@ export const deleteProduct= async (product)=>{
           body: JSON.stringify({
             id: product.id
           })
-  }).then(res => res.json()).then(data=> console.log(data)).catch(e=>'erro no deleteProduct');
+  }).then(res => res.json()).then(data=> {}).catch(e=>{});
 }
